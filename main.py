9bef07b31474d48f34fadf2d70ce59a4a90e0caa
beta_rx <beta.rx13@gmail.com>
2024-06-03 22:22:14 +0500

    frth commit Data Analysis and functions

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ df = df.drop_duplicates()
 df = df.fillna(0)
 # Showing number of rows and columns after cleaning
 print(df.shape)
+# The average life expectancy around the earth
+print("Average age is ", df["Value"].mean())
+# Finding minimum life expectancy at age 60 from dataset with "Value" column
+print("Minimum life expectancy at age 60 is ", df['Value'].min())
+# Finding maximum average age from dataset with "Value" column
+print("Maximum life expectancy age is ", df['Value'].max())
+
+# Determine the country with the highest life expectancy for a specific year
+year = 2019  # Specify the year
+highest_life_expectancy_country = df.loc[df['Period'] == year].nlargest(1, 'Value')['Location'].values[0]
+print("Country with the highest life expectancy age in 2019 is ", highest_life_expectancy_country)
+
+
+# checking for duplicates in 'Location' column
+duplicate_values = df['Location'].duplicated()
+print(duplicate_values)
+
+# grouping the data by continent
+gk = df.groupby('ParentLocation')
+print(gk.first())
